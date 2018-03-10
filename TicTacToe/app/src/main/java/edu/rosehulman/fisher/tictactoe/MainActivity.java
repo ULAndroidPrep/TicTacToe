515b694751455470337d7181f8c2fef8f2d080ff
David Fisher <fisherds@rose-hulman.edu>
2018-03-10 09:00:55 -0500

    updates to use a 1D array for buttons

--- TicTacToe/app/src/main/java/edu/rosehulman/fisher/tictactoe/MainActivity.java
+++ TicTacToe/app/src/main/java/edu/rosehulman/fisher/tictactoe/MainActivity.java
@@ -2,12 +2,58 @@ package edu.rosehulman.fisher.tictactoe;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+  private TicTacToeGame mGame;
+  private Button[] mButtons;
+  private TextView mGameStateTextView;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+    mGame = new TicTacToeGame(this);
+    mGameStateTextView = (TextView)findViewById(R.id.game_state_text_view);
+    mButtons = new Button[TicTacToeGame.NUM_SQUARES];
+    mButtons[0] = (Button)findViewById(R.id.button0);
+    mButtons[1] = (Button)findViewById(R.id.button1);
+    mButtons[2] = (Button)findViewById(R.id.button2);
+    mButtons[3] = (Button)findViewById(R.id.button3);
+    mButtons[4] = (Button)findViewById(R.id.button4);
+    mButtons[5] = (Button)findViewById(R.id.button5);
+    mButtons[6] = (Button)findViewById(R.id.button6);
+    mButtons[7] = (Button)findViewById(R.id.button7);
+    mButtons[8] = (Button)findViewById(R.id.button8);
+
+    // Fancy alternative
+//    for (int i = 0; i < TicTacToeGame.NUM_SQUARES; i++) {
+//        int id = getResources().getIdentifier("button" + i, "id", getPackageName());
+//        mButtons[i] = (Button)findViewById(id);
+//      }
+//    }
   }
+
+  public void pressedSquare(View view) {
+    mGame.pressedButtonAtIndex(Integer.valueOf((String) view.getTag()));
+    updateView();
+  }
+
+
+  public void pressedNewGame(View view) {
+    mGame = new TicTacToeGame(this);
+    updateView();
+  }
+
+  private void updateView() {
+    mGameStateTextView.setText(mGame.stringForGameState());
+
+    for (int i = 0; i < TicTacToeGame.NUM_SQUARES; i++) {
+      mButtons[i].setText(mGame.stringForButtonAtIndex(i));
+    }
+  }
+
 }
--- TicTacToe/app/src/main/java/edu/rosehulman/fisher/tictactoe/TicTacToeGame.java
+++ TicTacToe/app/src/main/java/edu/rosehulman/fisher/tictactoe/TicTacToeGame.java
@@ -2,146 +2,118 @@ package edu.rosehulman.fisher.tictactoe;
 
 import android.content.Context;
 import android.content.res.Resources;
-import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
 
 public class TicTacToeGame {
-	private enum GameState {
-		X_TURN,
-		O_TURN,
-		X_WIN,
-		O_WIN,
-		TIE_GAME
-	}
-	
-	private GameState gameState;
-	
-	private int[][] boardArray;
-	private Context context;
-	
-	public static final int NUM_ROWS = 3;
-	public static final int NUM_COLUMNS = 3;
-	
-	private static final int MARK_NONE = 0;
-	private static final int MARK_X = 1;
-	private static final int MARK_O = 2;
-	
-	public TicTacToeGame(Context context) {
-		this.context = context;
-		resetGame();
-	}
-	public void resetGame() {
-		this.boardArray = new int[NUM_ROWS][NUM_COLUMNS];
-		this.gameState = GameState.X_TURN;
-	}
-	public void pressedButtonAtLocation(int row, int column) {
-		if (row<0 || row>=NUM_ROWS || column<0 || column>=NUM_COLUMNS)
-			return;   // Not a valid square location
-		if (this.boardArray[row][column] != MARK_NONE)
-			return;   // Not empty
-		
-		if (this.gameState == GameState.X_TURN) {
-			this.boardArray[row][column] = MARK_X;
-			this.gameState = GameState.O_TURN;
-		} else if (this.gameState == GameState.O_TURN) {
-			this.boardArray[row][column] = MARK_O;
-			this.gameState = GameState.X_TURN;
-		}
-		checkForWin();
-	}
-	private void checkForWin() {
-		if (!(this.gameState == GameState.X_TURN || this.gameState == GameState.O_TURN))
-			return;
-		
-		if (didPieceWin(MARK_X)) {
-			this.gameState = GameState.X_WIN;
-		} else if (didPieceWin(MARK_O)) {
-			this.gameState = GameState.O_WIN;
-		} else if (isBoardFull()) {
-			
-			Log.d("TicTacToeGame", "The pattern is full!");
-			this.gameState = GameState.TIE_GAME;
-		}
-	}
-	private boolean isBoardFull() {
-		for (int row = 0 ; row<NUM_ROWS ; row++) {
-			for (int col = 0 ; col<NUM_COLUMNS ; col++) {
-				if (this.boardArray[row][col] == MARK_NONE) 
-				{
-					Log.d("TicTacToeGame", "Empty at Row: " + row +" Col: " + col);
-					return false;
-				}
-			}
-		}
-		return true;
-	}
-	private boolean didPieceWin(int markType) {
-		boolean allMarksMatch = true;
-		// Check all the columns for a win
-		for (int col=0 ; col<NUM_COLUMNS ; col++) {
-			allMarksMatch = true;
-			for (int row=0 ; row<NUM_ROWS ; row++) {
-				if (this.boardArray[row][col] != markType) {
-					allMarksMatch = false;
-					break;
-				}
-			}
-			if (allMarksMatch) return true;
-		}
-		
-		// Check all the rows for a win
-		for (int row=0 ; row<NUM_ROWS ; row++) {
-			allMarksMatch = true;
-			for (int col=0 ; col<NUM_COLUMNS ; col++) {
-				if (this.boardArray[row][col] != markType) {
-					allMarksMatch = false;
-					break;
-				}
-			}
-			if (allMarksMatch) return true;
-		}
-		
-		// Check down right diagonal
-		if (this.boardArray[0][0]==markType && this.boardArray[1][1]==markType && this.boardArray[2][2]==markType)
-			return true;
-		
-		// Check up right diagonal
-		if (this.boardArray[2][0]==markType && this.boardArray[1][1]==markType && this.boardArray[0][2]==markType)
-			return true;
-		
-		return false;
-	}
-	public String stringForButtonAtLocation(int row, int column) {
-		String label = "";
-		if (row>=0 && row<NUM_ROWS && column>=0 && column<NUM_COLUMNS)
-		{
-			if (this.boardArray[row][column] == MARK_X) {
-				return "X";
-			} else if (this.boardArray[row][column] == MARK_O) {
-				return "O";
-			}
-		}
-		return label;
-	}
-	public String stringForGameState() {
-		String gameStateLabel = "";
-	    Resources r = this.context.getResources();
-		switch (this.gameState) {
-			case X_TURN:
-				gameStateLabel = r.getString(R.string.x_turn);
-				break;
-			case O_TURN:
-				gameStateLabel = r.getString(R.string.o_turn);
-				break;
-			case X_WIN:
-				gameStateLabel = r.getString(R.string.x_win);
-				break;
-			case O_WIN:
-				gameStateLabel = r.getString(R.string.o_win);
-				break;
-			default:
-				gameStateLabel =  r.getString(R.string.tie_game);
-				break;
-		}
-		return gameStateLabel;
-	}
+  private enum MarkType {
+    NONE,
+    X,
+    O,
+  }
+  private enum GameState {
+    X_TURN,
+    O_TURN,
+    X_WIN,
+    O_WIN,
+    TIE_GAME
+  }
+  public static final int NUM_SQUARES = 9;
+
+  private GameState mGameState;
+  private MarkType[] mBoard;
+  private Context mContext;
+
+
+  public TicTacToeGame(Context context) {
+    this.mContext = context;
+    mBoard = new MarkType[NUM_SQUARES];
+    Arrays.fill(mBoard, MarkType.NONE);
+    mGameState = GameState.X_TURN;
+  }
+
+  public void pressedButtonAtIndex(int buttonIndex) {
+    if (buttonIndex < 0 || buttonIndex >= NUM_SQUARES) {
+      return;   // Not a valid square location
+    }
+    if (mBoard[buttonIndex] != MarkType.NONE) {
+      return;   // Not empty
+    }
+    if (mGameState == GameState.X_TURN) {
+      mBoard[buttonIndex] = MarkType.X;
+      mGameState = GameState.O_TURN;
+    } else if (mGameState == GameState.O_TURN) {
+      mBoard[buttonIndex] = MarkType.O;
+      mGameState = GameState.X_TURN;
+    }
+    checkForGameOver();
+  }
+
+  private void checkForGameOver() {
+    if (!(mGameState == GameState.X_TURN || mGameState == GameState.O_TURN)) {
+      return;  // The game is already over.
+    }
+    if (!Arrays.asList(mBoard).contains(MarkType.NONE)) {
+      mGameState = GameState.TIE_GAME;
+    }
+    ArrayList<String> linesOf3 = new ArrayList<>();
+    linesOf3.add(getMarkString(new int[]{0, 1, 2}));
+    linesOf3.add(getMarkString(new int[]{3, 4, 5}));
+    linesOf3.add(getMarkString(new int[]{6, 7, 8}));
+    linesOf3.add(getMarkString(new int[]{0, 3, 6}));
+    linesOf3.add(getMarkString(new int[]{1, 4, 7}));
+    linesOf3.add(getMarkString(new int[]{2, 5, 8}));
+    linesOf3.add(getMarkString(new int[]{0, 4, 8}));
+    linesOf3.add(getMarkString(new int[]{2, 4, 6}));
+    for (String lineOf3 : linesOf3) {
+      if (lineOf3.equals("XXX")) {
+        mGameState = GameState.X_WIN;
+      } else if (lineOf3.equals("OOO")) {
+        mGameState = GameState.O_WIN;
+      }
+    }
+  }
+
+  private String getMarkString(int[] indices) {
+    String markString = "";
+    for (int index : indices) {
+      markString += mBoard[index].name();
+    }
+    return markString;
+  }
+
+  public String stringForButtonAtIndex(int buttonIndex) {
+    if (buttonIndex < 0 || buttonIndex >= NUM_SQUARES) {
+      return "";   // Not a valid square location
+    }
+    if (mBoard[buttonIndex] == MarkType.NONE) {
+      return "";
+    }
+    return mBoard[buttonIndex].name();
+  }
+
+  public String stringForGameState() {
+    String gameStateLabel = "";
+    Resources r = mContext.getResources();
+    switch (mGameState) {
+      case X_TURN:
+        gameStateLabel = r.getString(R.string.x_turn);
+        break;
+      case O_TURN:
+        gameStateLabel = r.getString(R.string.o_turn);
+        break;
+      case X_WIN:
+        gameStateLabel = r.getString(R.string.x_win);
+        break;
+      case O_WIN:
+        gameStateLabel = r.getString(R.string.o_win);
+        break;
+      default:
+        gameStateLabel =  r.getString(R.string.tie_game);
+        break;
+    }
+    return gameStateLabel;
+  }
 }
